b6597f905bb16a587abc32ac6405947efce41d37
nkloga <n.kloga@gmail.com>
2019-02-19 11:37:05 +0200

    second commit

--- src/main/java/greeting/Application.java
+++ src/main/java/greeting/Application.java
@@ -2,17 +2,11 @@ package greeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
-    }
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
